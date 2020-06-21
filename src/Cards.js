b2fbaca9c2175095ed47cb7628b1149d6045b6b1
Abhishekiet <62215477+Abhishekiet@users.noreply.github.com>
2020-06-21 14:29:02 +0530

    Update Cards.js

--- src/Cards.js
+++ src/Cards.js
@@ -9,12 +9,14 @@ class Cards extends Component{
     {   const {video,cardclick} = this.props
         
         return(
-        <div >
+        <div className="p-2">
            <Card style={{ width: '18rem' }} className="shadow m-2" onClick={cardclick}>
               <Card.Img variant="top" src={video.snippet.thumbnails.medium.url} />
            
             <Card.Body>
                  <Card.Title>{video.snippet.title}</Card.Title>
+                  <div className="text-muted"> {video.snippet.channelTitle} </div>
+
             </Card.Body>
            
            </Card>
